1c6e3d68ef6d6b5474d4f4a8d9eb5b22c2d2bff5
GuilhermeBenites <guilherme.rebello91@gmail.com>
2016-08-26 14:47:05 -0400

    Proposta de módulo novo de Relatórios

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -101,6 +101,14 @@
                   </li>
                 </ul>
               </li>
+              <li><a><i class="fa fa-pie-chart"></i> Relatórios <span class="fa fa-chevron-down"></span></a>
+                <ul class="nav child_menu">
+                  <li><a href="#">Venda</a>
+                  </li>
+                  <li><a href="#">Estoque</a>
+                  </li>
+                </ul>
+              </li>
             </ul>
           </div>
 
